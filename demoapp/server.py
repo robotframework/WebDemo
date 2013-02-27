14682cfccfb024d710fbb0b753ee7ca45a798431
Pekka Klärck <peke@iki.fi>
2013-02-27 15:24:04 +0200

    server: better handling of ctrl-c (no traceback) and allow re-binding address
    Signed-off-by: Pekka Klärck <peke@iki.fi>

--- demoapp/server.py
+++ demoapp/server.py
@@ -5,8 +5,8 @@
 Usage:  server.py [port]
 
 This server serves HTML pages under `html` directory. Server is started simply
-by running this script from the command line or double-clicking it in file
-manager. In the former case the server can be closed using Ctrl-C and in the
+by running this script from the command line or double-clicking it in a file
+manager. In the former case the server can be shut down with Ctrl-C and in the
 latter case by closing the opened window.
 
 By default the server uses port 7272, but a custom port can be given as
@@ -23,22 +23,33 @@ ROOT = join(dirname(abspath(__file__)), 'html')
 PORT = 7272
 
 
-class RequestHandler(SimpleHTTPRequestHandler):
+class DemoServer(TCPServer):
+    allow_reuse_address = True
+
+    def __init__(self, port=PORT):
+        TCPServer.__init__(self, ('localhost', int(port)), PostingHandler)
+
+    def serve(self, directory=ROOT):
+        chdir(directory)
+        print 'Demo server starting on port %d.' % self.server_address[1]
+        try:
+            server.serve_forever()
+        except KeyboardInterrupt:
+            server.server_close()
+        print 'Demo server stopped.'
+
+
+class PostingHandler(SimpleHTTPRequestHandler):
 
     def do_POST(self):
         self.do_GET()
 
 
-def start_server(port=PORT):
-    httpd = TCPServer(('localhost', int(port)), RequestHandler)
-    chdir(ROOT)
-    print 'Demo application starting on port %s.' % port
-    httpd.serve_forever()
-
-
 if __name__ == '__main__':
     import sys
     try:
-        start_server(*sys.argv[1:])
+        server = DemoServer(*sys.argv[1:])
     except (TypeError, ValueError):
         print __doc__
+    else:
+        server.serve()
