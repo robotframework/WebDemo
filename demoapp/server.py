68811d370f6d5e02f9fc3c1470bd675115f5ca21
Pekka Klärck <peke@iki.fi>
2013-02-27 14:17:51 +0200

    simplified starting server. no need for rundemo.py anymore.
    Signed-off-by: Pekka Klärck <peke@iki.fi>

--- demoapp/server.py
+++ demoapp/server.py
@@ -1,84 +1,44 @@
 #!/usr/bin/env python
 
-#  Copyright 2008-2011 Nokia Siemens Networks Oyj
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
+"""Simple HTTP server for Robot Framework web testing demo.
 
-"""Simple HTTP server requiring only Python and no other preconditions.
+Usage:  server.py [port]
 
-Server is started by running this script with argument 'start' and
-optional port number (default port 7272). Server root is the same
-directory where this script is situated. Server can be stopped either
-using Ctrl-C or running this script with argument 'stop' and same port
-number as when starting it.
+This server serves HTML pages under `html` directory. Server is started simply
+by running this script from the command line or double-clicking it in file
+manager. In the former case the server can be closed using Ctrl-C and in the
+latter case by closing the opened window.
+
+By default the server uses port 7272, but a custom port can be given as
+an argument from the command line.
 """
 
-import os
-import sys
-import httplib
-import BaseHTTPServer
-import SimpleHTTPServer
+from os import chdir
+from os.path import abspath, dirname, join
+from SocketServer import TCPServer
+from SimpleHTTPServer import SimpleHTTPRequestHandler
 
 
-DEFAULT_PORT = 7272
+ROOT = join(dirname(abspath(__file__)), 'html')
+PORT = 7272
 
 
-class StoppableHttpServer(BaseHTTPServer.HTTPServer):
-
-    def serve_forever(self):
-        self.stop = False
-        while not self.stop:
-            try:
-                self.handle_request()
-            except KeyboardInterrupt:
-                break
-
-
-class StoppableHttpRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
-
-    def do_QUIT(self):
-        self.send_response(200)
-        self.end_headers()
-        self.server.stop = True
+class RequestHandler(SimpleHTTPRequestHandler):
 
     def do_POST(self):
-        # We could also process paremeters here using something like below.
-        # length = self.headers['Content-Length']
-        # print self.rfile.read(int(length))
         self.do_GET()
 
 
-def start_server(port=DEFAULT_PORT):
-    print "Demo application starting on port %s" % port
-    root  = os.path.dirname(os.path.abspath(__file__))
-    os.chdir(root)
-    server = StoppableHttpServer(('localhost', int(port)), StoppableHttpRequestHandler)
-    server.serve_forever()
-
-def stop_server(port=DEFAULT_PORT):
-    print "Demo application on port %s stopping" % port
-    conn = httplib.HTTPConnection("localhost:%s" % port)
-    conn.request("QUIT", "/")
-    conn.getresponse()
-
-def print_help():
-    print __doc__
+def start_server(port=PORT):
+    httpd = TCPServer(('localhost', int(port)), RequestHandler)
+    chdir(ROOT)
+    print 'Demo application starting on port %s.' % port
+    httpd.serve_forever()
 
 
 if __name__ == '__main__':
+    import sys
     try:
-        {'start': start_server,
-         'stop': stop_server,
-         'help': print_help}[sys.argv[1]](*sys.argv[2:])
-    except (IndexError, KeyError, TypeError):
-        print 'Usage: %s start|stop|help [port]' % os.path.basename(sys.argv[0])
+        start_server(*sys.argv[1:])
+    except (TypeError, ValueError):
+        print __doc__
