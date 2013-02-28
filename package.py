e68b30e13ebfe5cf3481707ab98c17bc7b69cd3c
Pekka Klärck <peke@iki.fi>
2013-02-28 11:52:27 +0200

    packaging: use WebDemo as the package basename and date as the version. also considerably simpler implementation.
    Signed-off-by: Pekka Klärck <peke@iki.fi>

--- package.py
+++ package.py
@@ -1,43 +1,24 @@
 #!/usr/bin/env python
 
-import os, shutil
+from glob import  glob
+from os import chdir, sep as SEP
+from os.path import abspath, dirname, join
+from time import localtime
 from zipfile import ZipFile, ZIP_DEFLATED
 
-VERSION = "1.0"
-THIS_DIR = os.path.dirname(os.path.abspath(__file__))
-#execfile(os.path.join(THIS_DIR, '..', 'src', 'Selenium2Library', 'version.py'))
+NAME = 'WebDemo'
+ZIP_NAME = NAME + '-%d%02d%02d.zip' % localtime()[:3]
+FILES = ['README.rst',
+         'demoapp/server.py',
+         'demoapp/html/*.html',
+         'demoapp/html/*.css',
+         'login_tests/*.txt']
 
-FILES = {
-    '': ['rundemo.py'],
-    'testcases': ['valid_login.txt', 'invalid_login.txt','content_check.txt','resource.txt'],
-    'demoapp': ['server.py'],
-    'demoapp/html': ['index.html', 'welcome.html', 'error.html', 'demo.css', 'file_information.txt', 'file_names.txt', 'file_places.txt']
-}
-
-def main():
-    cwd = os.getcwd()
-    try:
-        os.chdir(THIS_DIR)
-        name = 'robotframework-selenium2library-%s-demo' % VERSION
-        zipname = '%s.zip' % name
-        if os.path.exists(zipname):
-            os.remove(zipname)
-        zipfile = ZipFile(zipname, 'w', ZIP_DEFLATED)
-        for dirname in FILES:
-            for filename in FILES[dirname]:
-                path = os.path.join('.', dirname.replace('/', os.sep), filename)
-                print 'Adding:  ', os.path.normpath(path)
-                zipfile.write(path, os.path.join(name, path))
-        zipfile.close()
-        target_path = os.path.join('.', 'dist')
-        if os.path.exists(target_path):
-            shutil.rmtree(target_path, ignore_errors=True)
-        os.makedirs(target_path)
-        os.rename(zipname, os.path.join(target_path, zipname))
-        print 'Created: ', os.path.abspath(target_path)
-    finally:
-        os.chdir(cwd)
-
-
-if __name__ == '__main__':
-    main()
+chdir(dirname(abspath(__file__)))
+zipfile = ZipFile(ZIP_NAME, 'w', ZIP_DEFLATED)
+for pattern in FILES:
+    for path in glob(pattern.replace('/', SEP)):
+        print 'Adding:  ', path
+        zipfile.write(path, join(NAME, path))
+zipfile.close()
+print 'Created: ', ZIP_NAME
