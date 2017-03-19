c340a70b893bb4c0297b58c3e88f40dab2f52930
Pekka Klärck <peke@iki.fi>
2017-03-19 22:36:03 +0200

    Handle more than one simultaneous connection.
    Important if a browser leaves connection open. Seems that browsers
    nowadays do that sometimes.
    
    Also Python 3 compatibility.

--- demoapp/server.py
+++ demoapp/server.py
@@ -13,30 +13,38 @@ By default the server uses port 7272, but a custom port can be given as
 an argument from the command line.
 """
 
+from __future__ import print_function
+
 from os import chdir
 from os.path import abspath, dirname, join
-from SocketServer import TCPServer
-from SimpleHTTPServer import SimpleHTTPRequestHandler
+try:
+    from SocketServer import ThreadingMixIn
+    from BaseHTTPServer import HTTPServer
+    from SimpleHTTPServer import SimpleHTTPRequestHandler
+except ImportError:
+    from socketserver import ThreadingMixIn
+    from http.server import SimpleHTTPRequestHandler, HTTPServer
 
 
 ROOT = join(dirname(abspath(__file__)), 'html')
 PORT = 7272
 
 
-class DemoServer(TCPServer):
+class DemoServer(ThreadingMixIn, HTTPServer):
     allow_reuse_address = True
 
     def __init__(self, port=PORT):
-        TCPServer.__init__(self, ('localhost', int(port )), SimpleHTTPRequestHandler)
+        HTTPServer.__init__(self, ('localhost', int(port)),
+                            SimpleHTTPRequestHandler)
 
     def serve(self, directory=ROOT):
         chdir(directory)
-        print 'Demo server starting on port %d.' % self.server_address[1]
+        print('Demo server starting on port %d.' % self.server_address[1])
         try:
             server.serve_forever()
         except KeyboardInterrupt:
             server.server_close()
-        print 'Demo server stopped.'
+        print('Demo server stopped.')
 
 
 if __name__ == '__main__':
@@ -44,6 +52,6 @@ if __name__ == '__main__':
     try:
         server = DemoServer(*sys.argv[1:])
     except (TypeError, ValueError):
-        print __doc__
+        print(__doc__)
     else:
         server.serve()
